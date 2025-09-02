cc565f2a1134d5b58296d7142829615922f51c27
juliiirnr-hash <juliiirnr@gmail.com>
2025-09-02 18:07:21 -0300

    Cambios 9

--- #Ejercicio4.9.1.py
+++ #Ejercicio4.9.1.py
@@ -2,7 +2,7 @@
 # Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la media de esos valores
 suma=0
 n=0
-for i in range(6):
+for i in range(100):
     numero=int(input("Ingrese un número entero: "))
     suma+=numero
     n+=1
